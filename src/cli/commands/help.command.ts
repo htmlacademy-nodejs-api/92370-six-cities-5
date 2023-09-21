126918008ee42bd8519ab96631cc3f041fdb0943
colorkid <colorkid@yandex.ru>
2023-09-21 10:21:21 +0300

    Implements CLI methods: version, help, import.

---
+++ src/cli/commands/help.command.ts
@@ -0,0 +1,22 @@
+import { Command } from './command.interface.js';
+import chalk from 'chalk';
+
+export class HelpCommand implements Command {
+  public getName(): string {
+    return '--help';
+  }
+
+  public async execute(..._parameters: string[]): Promise<void> {
+    console.info(`
+      ${chalk.blue('Программа для подготовки данных для REST API сервера.')}
+
+      Пример: cli.js --<command> [--arguments]
+
+      Команды:
+
+        ${chalk.green('--version:                   # выводит номер версии')}
+        ${chalk.red('--help:                      # печатает этот текст')}
+        ${chalk.bgGreen('--import <path>:             # импортирует данные из TSV')}
+  `);
+  }
+}
